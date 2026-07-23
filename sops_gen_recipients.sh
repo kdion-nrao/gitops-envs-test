af125b17da1c7774b79812e9a0e56e54db8f73d5
Kevin Dion <kdion@nrao.edu>
2026-07-23 09:50:21 -0400

    ARC update token secret

--- sops_gen_recipients.sh
+++ sops_gen_recipients.sh
@@ -83,18 +83,12 @@ do
     key="$key" yq -i '.creation_rules[].age += strenv(key)' .sops.yaml
 done
 
-echo "=== Encrypting secrets"
-readarray -d '' secrets_files < <(find . -name '*secret*.yaml' ! -name '*.enc.yaml' -print0)
-for plain_file in "${secrets_files[@]}"
+## Re-encrypt secret files
+echo "=== Updating keys "
+shopt -s globstar nullglob
+for file in ./**/*secret*.enc.yaml
 do
-    enc_file="${plain_file%.yaml}.enc.yaml"
-    if [ -f "$enc_file" ]
-    then
-        sops updatekeys -y "$enc_file"
-    else
-        echo "Encrypting $plain_file"
-        sops -e "$plain_file" > "$enc_file"
-    fi
+    sops updatekeys -y "$file"
 done
 
 echo "=== Done!"
